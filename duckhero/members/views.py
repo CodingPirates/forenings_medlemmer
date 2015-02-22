6860207407190c7d8723e6aac6c367b2e1500275
Goblin <duckhero@gmail.com>
2015-02-22 12:30:52 +0100

    Fixed bug on create
    now redirects properly to /family/<uuid>

--- duckhero/members/views.py
+++ duckhero/members/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse_lazy, reverse
 from members.models import Person, Family
 
 class FamilyCreate(CreateView):
     model=Family
     fieds=['email']
+    def get_success_url(self):
+        return 'family/{}'.format(self.object.unique)
 
 class FamilyUpdate(UpdateView):
     fields=['email']
