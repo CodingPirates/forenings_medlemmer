fc6f34198e005d264d60bf468a5793593d9fa14f
Goblin <duckhero@gmail.com>
2015-02-28 11:20:54 +0100

    Corrected regex expressions in urls

--- members/urls.py
+++ members/urls.py
@@ -3,7 +3,7 @@ from members.views import FamilyCreate, Details, PersonCreate, PersonUpdate
 
 urlpatterns = patterns('',
     url(r'^$', FamilyCreate.as_view(), name='family_add'),
-    url(r'(?P<unique>[\w-]+)/Person/?P<pk>/$', PersonUpdate.as_view(), name='person_update'),
+    url(r'(?P<unique>[\w-]+)/Person/(?P<pk>[\d])/$', PersonUpdate.as_view(), name='person_update'),
     url(r'(?P<unique>[\w-]+)/Person/$', PersonCreate.as_view(), name='person_add'),
-    url(r'(?P<unique>[\w-]+)/$', Details, name='family_detail'),
+    url(r'(?P<unique>[\w-]+)$', Details, name='family_detail'),
 )
