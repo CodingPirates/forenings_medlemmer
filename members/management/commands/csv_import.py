44ec23b0ef614abd041490efdde3df1f39d151df
Per Hansen <nallic@users.noreply.github.com>
2015-07-16 22:56:01 +0200

    added date format for Aarhus memberlist

--- members/management/commands/csv_import.py
+++ members/management/commands/csv_import.py
@@ -16,6 +16,7 @@ class Command(BaseCommand):
         make_option('-e', '--email_column', help="column which contains the email of parent who has signed up. (First column is specified as 0)", type="int", default=4, metavar='column'),
         make_option('-n', '--name_column', help="column which contains the name of member who has signed up. (First column is specified as 0)", type="int", default=1, metavar='column'),
         make_option('-l', '--first_row_is_label', help="The first row in CSV file is expected to be column labels", action="store_true", default=False),
+        make_option('-f', '--date_format', help="Date format (look in sourcecode)", type="int", default=0, metavar='format'),
         )
     args = 'csvfile'
 
@@ -66,7 +67,30 @@ class Command(BaseCommand):
             for col in range(columns):
                 journal = journal + labels[col] + ': ' + entry[col] + '\n'
 
-            date = datetimeobject.strptime(entry[options['date_column']] + " +0001", '%m/%d/%Y %H:%M:%S %z')
+
+            if(options['date_format'] == 0):
+                date_format = '%m/%d/%Y %H:%M:%S %z' # '3/20/2014 19:51:32'
+                date = datetimeobject.strptime(entry[options['date_column']].lstrip() + " +0001", date_format)
+            if(options['date_format'] == 1):
+                date_format = '%d. %m %Y, %H:%M %z' # '12. december 2014, 17:58'
+                months = [
+                    ('januar' , '1'),
+                    ('februar' , '2'),
+                    ('marts' , '3'),
+                    ('april' , '4'),
+                    ('maj' , '5'),
+                    ('juni' , '6'),
+                    ('juli' , '7'),
+                    ('august' , '8'),
+                    ('september' , '9'),
+                    ('oktober' , '10'),
+                    ('november' , '11'),
+                    ('december' , '12'),]
+
+                date_string = entry[options['date_column']].lstrip()
+                for month_name, month_number in months:
+                    date_string = date_string.replace(month_name, month_number)
+                date = datetimeobject.strptime(date_string + " +0001", date_format)
 
             # find or create the family
             family, created = Family.objects.get_or_create(email = email)
