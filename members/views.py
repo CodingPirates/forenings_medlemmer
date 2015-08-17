7b882f13f1511f16a4d4a91fd558c2c456c82427
Per Hansen <nallic@users.noreply.github.com>
2015-08-17 22:28:50 +0200

    Anonymize public waitinglist.
    Family page now has link to waiting list, showing own childrens positions.

--- members/views.py
+++ members/views.py
@@ -473,24 +473,35 @@ def QuickpayCallback(request):
         return HttpResponseForbidden('Invalid request')
 
 
-def waitinglistView(request):
+def waitinglistView(request, unique=None):
 
     department_children_waiting = {'departments': {}}
     department_loop_counter=0
-    for department in Department.objects.filter(has_waiting_list = True).order_by('zipcode'):
+    #deparments_query = Department.objects.filter(has_waiting_list = True).order_by('zipcode').filter(waitinglist__person__family__unique=unique)
+    deparments_query = Department.objects.filter(has_waiting_list = True).order_by('zipcode')
+
+    for department in deparments_query:
         department_children_waiting['departments'][department_loop_counter] = {}
         department_children_waiting['departments'][department_loop_counter]['name'] = department.name
         department_children_waiting['departments'][department_loop_counter]['waiting'] = {}
 
-        waiting_in_department = WaitingList.objects.filter(department__pk=department.pk).select_related('person').order_by('on_waiting_list_since')
+        waiting_in_department = WaitingList.objects.filter(department__pk=department.pk).select_related('person', 'person__family').order_by('on_waiting_list_since')
+
         child_loop_counter=1
         for waiting in waiting_in_department:
             department_children_waiting['departments'][department_loop_counter]['waiting'][child_loop_counter] = {}
-            department_children_waiting['departments'][department_loop_counter]['waiting'][child_loop_counter]['firstname'] = waiting.person.firstname()
-            department_children_waiting['departments'][department_loop_counter]['waiting'][child_loop_counter]['zipcode'] = waiting.person.zipcode
+            if(waiting.person.family.unique == unique):
+                department_children_waiting['departments'][department_loop_counter]['waiting'][child_loop_counter]['firstname'] = waiting.person.firstname()
+                department_children_waiting['departments'][department_loop_counter]['waiting'][child_loop_counter]['zipcode'] = waiting.person.zipcode
+                department_children_waiting['departments'][department_loop_counter]['waiting'][child_loop_counter]['color'] = True
+            else:
+                department_children_waiting['departments'][department_loop_counter]['waiting'][child_loop_counter]['firstname'] = '-'
+                department_children_waiting['departments'][department_loop_counter]['waiting'][child_loop_counter]['zipcode'] = waiting.person.zipcode
+                department_children_waiting['departments'][department_loop_counter]['waiting'][child_loop_counter]['color'] = False
+
             department_children_waiting['departments'][department_loop_counter]['waiting'][child_loop_counter]['added'] = waiting.person.added
             child_loop_counter = child_loop_counter + 1
         department_loop_counter = department_loop_counter + 1
 
 
-    return render(request, 'members/waitinglist.html', {'department_children_waiting': department_children_waiting})
+    return render(request, 'members/waitinglist.html', {'department_children_waiting': department_children_waiting, 'unique': unique})
