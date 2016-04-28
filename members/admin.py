24762e5b65065634a219e5cf990bd18fdc9b52dc
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2016-04-28 22:57:25 +0200

    Correct which fields admin have

--- members/admin.py
+++ members/admin.py
@@ -155,7 +155,7 @@ class PaymentInline(admin.TabularInline):
 
 class VolunteerInline(admin.TabularInline):
     model = Volunteer
-    fields = ('department','has_certificate','added','removed','approved')
+    fields = ('department','added','removed','approved')
     extra = 0
 
 class ActivityParticipantInline(admin.TabularInline):
