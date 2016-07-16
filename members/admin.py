24c1f87ad46fc1ec4b4db2b09487ff1988cff21b
Kristoffer Rath Hansen <kristoffer@rathhansen.com>
2016-07-16 14:42:00 +0200

    Make result on volunteers instinct

--- members/admin.py
+++ members/admin.py
@@ -491,9 +491,9 @@ class VolunteerListFilter(admin.SimpleListFilter):
         # to decide how to filter the queryset.
 
         if self.value() == 'any':
-            return queryset.filter(volunteer__isnull=False).exclude(volunteer__removed__isnull=False)
+            return queryset.filter(volunteer__isnull=False).exclude(volunteer__removed__isnull=False).distinct()
         elif self.value() == 'none':
-            return queryset.filter(Q(volunteer__isnull=True) | Q(volunteer__removed__isnull=False))
+            return queryset.filter(Q(volunteer__isnull=True) | Q(volunteer__removed__isnull=False)).distinct()
         elif self.value() == None:
             return queryset
         else:
