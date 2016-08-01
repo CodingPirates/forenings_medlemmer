69ab858f8d1f7676b7576262e208a11f83e7496c
Kristoffer Rath Hansen <kristoffer@rathhansen.com>
2016-08-01 02:13:16 +0200

    Volunteer filter now works!

--- members/admin.py
+++ members/admin.py
@@ -491,9 +491,9 @@ class VolunteerListFilter(admin.SimpleListFilter):
         # to decide how to filter the queryset.
 
         if self.value() == 'any':
-            return queryset.filter(volunteer__isnull=False).exclude(volunteer__removed__isnull=False).distinct()
+            return queryset.filter(volunteer__isnull=False).filter(volunteer__removed__isnull=True).distinct()
         elif self.value() == 'none':
-            return queryset.filter(Q(volunteer__isnull=True) | Q(volunteer__removed__isnull=False)).distinct()
+            return queryset.filter(volunteer__isnull=True) | queryset.exclude(volunteer__removed__isnull=True).distinct()
         elif self.value() == None:
             return queryset
         else:
