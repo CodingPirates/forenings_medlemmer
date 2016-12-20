b5ec98493b8c88556ca424178ba898593c6ceab9
Per Hansen <per@kofod-hansen.com>
2016-12-20 18:53:48 +0100

    Et par praktiske opslags queries.

--- lookups.sql
+++ lookups.sql
@@ -16,6 +16,26 @@ SELECT zr.region, COUNT(DISTINCT p.id) FROM members_person AS p
   WHERE pay.refunded_dtm IS NULL AND pay.amount_ore > 7500
   GROUP BY zr.region;
 
+# members pr. lokalforening 2016
+SELECT u.name, COUNT(DISTINCT p.id) FROM members_person AS p
+  JOIN members_payment AS pay ON pay.person_id=p.id
+  JOIN members_zipcoderegion AS zr ON zr.zipcode=p.zipcode
+  JOIN members_activity AS a ON pay.activity_id=a.id
+  JOIN members_department AS d ON a.department_id=d.id
+  JOIN members_union AS u ON u.id=d.union_id
+  WHERE pay.refunded_dtm IS NULL AND pay.amount_ore > 7500 AND pay.added > date('2016-01-01')
+  GROUP BY u.id;
+
+# members pr. afdeling 2016
+SELECT d.name, u.name, COUNT(DISTINCT p.id) FROM members_person AS p
+  JOIN members_payment AS pay ON pay.person_id=p.id
+  JOIN members_zipcoderegion AS zr ON zr.zipcode=p.zipcode
+  JOIN members_activity AS a ON pay.activity_id=a.id
+  JOIN members_department AS d ON a.department_id=d.id
+  JOIN members_union AS u ON u.id=d.union_id
+  WHERE pay.refunded_dtm IS NULL AND pay.amount_ore > 7500 AND pay.added > date('2016-01-01')
+  GROUP BY d.id;
+    
 # Medlemmer (pr. region) 2016
 SELECT zr.region, COUNT(DISTINCT p.id) FROM members_person AS p
   JOIN members_payment AS pay ON pay.person_id=p.id
@@ -35,3 +55,16 @@ SELECT zr.region, SUM(pay.amount_ore/100) FROM members_person AS p
   JOIN members_zipcoderegion AS zr ON zr.zipcode=p.zipcode
   WHERE pay.refunded_dtm IS NULL
   GROUP BY zr.region;
+
+# Activity age distribution
+SELECT p.birthday FROM members_person AS p
+  JOIN members_member AS m ON m.person_id=p.id
+  JOIN members_activityparticipant AS ap ON ap.member_id=m.id
+  WHERE ap.activity_id=160;
+
+# Deltagerliste
+SELECT p.name, f.email, ap.note FROM members_activityparticipant AS ap
+JOIN members_member AS m ON ap.member_id=m.id
+JOIN members_person AS p ON p.id=m.person_id
+JOIN members_family AS f ON p.family_id=f.id
+WHERE ap.activity_id IS 160;
