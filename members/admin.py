fbc253edb1da82e8cf09f397168b74cda91f8091
Per Hansen <per@kofod-hansen.com>
2017-03-07 07:20:38 +0100

    Made person, family and participant searchable fields in payments, to avoid long selection lists. (close #118)

--- members/admin.py
+++ members/admin.py
@@ -766,6 +766,7 @@ admin.site.register(User, UserAdmin)
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ['pk', 'added', 'payment_type', 'amount_ore', 'family', 'confirmed_dtm', 'cancelled_dtm', 'rejected_dtm', 'activityparticipant']
     list_filter = ['payment_type', 'activity']
+    raw_id_fields = ('person', 'activityparticipant', 'family')
     date_hierarchy = 'added'
     search_fields = ('family__email',)
     select_related = ('activityparticipant')
