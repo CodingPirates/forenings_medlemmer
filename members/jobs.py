2e7ca349c0fe1d0cde56b6c7a4b993e10849e794
Per Kofod Hansen <per@kofod-hansen.com>
2017-06-07 22:00:35 +0200

    Added statistics job.
    Daily job, to calculate some statistics.
    
    Added "added_dtm" to waiting list items. Migration copies from "on_waiting_list_since"

--- members/jobs.py
+++ members/jobs.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django_cron import CronJobBase, Schedule
-from members.models import EmailItem, Family, Notification, EmailTemplate, ActivityParticipant, Payment
+from members.models import EmailItem, Family, Notification, EmailTemplate, ActivityParticipant, Payment, \
+    DailyStatisticsDepartment, DailyStatisticsGeneral, Person, WaitingList, Department, Union, Activity
 from django.db.models import Q, F
+from django.db.models import Count, Avg
 import datetime
 from django.utils import timezone
 
@@ -75,6 +77,69 @@ class PollQuickpayPaymentsCronJob(CronJobBase):
         for payment in payments:
             payment.get_quickpaytransaction().update_status()
 
+
+# Daily statistics job
+class GenerateStatisticsCronJob(CronJobBase):
+    RUN_AT_TIMES = ['23:59', ]
+
+    schedule = Schedule(run_at_times=RUN_AT_TIMES)
+    code = 'members.generate_statistics_cronjob'  # a unique code
+
+
+    def do(self):
+        timestamp = timezone.now()  # make sure all entries share same timestamp
+
+        # generate general statistics
+        dailyStatisticsGeneral = DailyStatisticsGeneral()
+
+        dailyStatisticsGeneral.timestamp = timestamp
+        dailyStatisticsGeneral.persons = Person.objects.count()
+        dailyStatisticsGeneral.children_male = Person.objects.filter(membertype=Person.CHILD, gender=Person.MALE).count()
+        dailyStatisticsGeneral.children_female = Person.objects.filter(membertype=Person.CHILD, gender=Person.FEMALE).count()
+        dailyStatisticsGeneral.children = dailyStatisticsGeneral.children_male + dailyStatisticsGeneral.children_female
+        dailyStatisticsGeneral.volunteers_male = Person.objects.filter(gender=Person.MALE, volunteer__isnull=False).count()
+        dailyStatisticsGeneral.volunteers_female = Person.objects.filter(gender=Person.FEMALE, volunteer__isnull=False).count()
+        dailyStatisticsGeneral.volunteers = dailyStatisticsGeneral.volunteers_male + dailyStatisticsGeneral.volunteers_female
+        dailyStatisticsGeneral.departments = Department.objects.filter(closed_dtm=None).count()
+        dailyStatisticsGeneral.unions = Union.objects.count()
+        dailyStatisticsGeneral.waitinglist = WaitingList.objects.all().aggregate(Count('person', distinct=True))['person__count']
+        dailyStatisticsGeneral.family_visits = Family.objects.filter(last_visit_dtm__gt=(timestamp-datetime.timedelta(days=1))).count()
+        dailyStatisticsGeneral.dead_profiles = Family.objects.filter(last_visit_dtm__lt=(timestamp-datetime.timedelta(days=365))).count()
+        dailyStatisticsGeneral.current_activity_participants = Person.objects.filter(member__activityparticipant__activity__end_date__gt=timestamp).distinct().count()
+        dailyStatisticsGeneral.activity_participants = Person.objects.filter(member__activityparticipant__activity__isnull=False).distinct().count()
+
+        dailyStatisticsGeneral.save()
+
+        # generate daily department statistics
+        departments = Department.objects.filter(closed_dtm=None)
+        for department in departments:
+            dailyStatisticsDepartment = DailyStatisticsDepartment()
+
+            dailyStatisticsDepartment.timestamp = timestamp
+            dailyStatisticsDepartment.department = department
+            dailyStatisticsDepartment.active_activities = Activity.objects.filter(department=department, end_date__gt=timestamp).count()
+            dailyStatisticsDepartment.activities = Activity.objects.filter(department=department).count()
+            dailyStatisticsDepartment.current_activity_participants = 0
+            dailyStatisticsDepartment.current_activity_participants_age_avg = 0
+            dailyStatisticsDepartment.current_activity_participants_age_min = 0
+            dailyStatisticsDepartment.current_activity_participants_age_max = 0
+            dailyStatisticsDepartment.activity_participants = 0
+            dailyStatisticsDepartment.members = 0
+            dailyStatisticsDepartment.waitinglist = WaitingList.objects.filter(department=department).count()
+            firstWaitingListItem = WaitingList.objects.filter(department=department).order_by('on_waiting_list_since').first()
+            if firstWaitingListItem:
+                dailyStatisticsDepartment.waitingtime = timestamp.date() - firstWaitingListItem.on_waiting_list_since
+            else:
+                dailyStatisticsDepartment.waitingtime = datetime.timedelta(days=0)
+            dailyStatisticsDepartment.payments = 0
+            dailyStatisticsDepartment.volunteers_male = 0
+            dailyStatisticsDepartment.volunteers_female = 0
+            dailyStatisticsDepartment.volunteers = 0
+            dailyStatisticsDepartment.volunteers_age_avg = 0
+            dailyStatisticsDepartment.volunteers_age_min = 0
+            dailyStatisticsDepartment.volunteers_age_max = 0
+
+            dailyStatisticsDepartment.save()
 
 # TODO:Find created families/persons, which never clicked the link (spambots etc.)
 
