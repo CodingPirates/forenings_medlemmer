6b2b8faf802cfbab0fd257a752dfc7a3705f4d44
Jacob Jenner Rasmussen <jacob@jener.dk>
2017-06-25 21:46:18 +0200

    Revert "Create a static default unique value for Family model"
    This reverts commit 87a0ed36e695073b1a5733e0511ec9e7ecba6ea2.

--- members/models/family.py
+++ members/models/family.py
@@ -15,7 +15,7 @@ class Family(models.Model):
             ("view_family_unique", "Can view family UUID field (password) - gives access to address"),
         )
 
-    unique = models.UUIDField(default=uuid.UUID('00000000-0000-0000-0000-000000000000'))
+    unique = models.UUIDField(default=uuid.uuid4)
     email = models.EmailField(unique=True)
     dont_send_mails = models.BooleanField('Vil ikke kontaktes', default=False)
     updated_dtm = models.DateTimeField('Opdateret', auto_now=True)
