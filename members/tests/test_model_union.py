3fba5448b961cc6f09c36c32c1cb84a3456ab49a
Jacob Jenner Rasmussen <jacob@jener.dk>
2017-07-12 12:21:45 +0200

    Added testcase for the Union model
    Test validation works correctly.

---
+++ members/tests/test_model_union.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from members.models.union import Union
+from django.core.exceptions import ValidationError
+
+
+class TestModelUnion(TestCase):
+    def test_clean_undefined_bank(self):
+        # Validation should fail if the union doesn't have a bank
+        union = Union(bank_main_org=False)
+        with self.assertRaises(ValidationError):
+            union.clean()
+
+        # If we set the bank it shouldn't fail
+        # ToDo: maybe test some differently formatted bank accounts
+        union.bank_account = "1234-1234567890"
+        union.clean()
+
+        union.bank_main_org = True
+        union.clean()
