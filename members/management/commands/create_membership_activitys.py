27446f49865fa88d576561859e83440658304211
Per Kofod Hansen <per@kofod-hansen.com>
2018-01-28 18:48:34 +0100

    Added union field to activitys and made this field set to the correct union on membership activitys. Updated text to activity confirmed text.

--- members/management/commands/create_membership_activitys.py
+++ members/management/commands/create_membership_activitys.py
@@ -11,41 +11,34 @@ class Command(BaseCommand):
         mainDepartment = Department.objects.get(pk=30)
 
         for curUnion in Union.objects.all():
+            if curUnion.id == 1:
+                print("springer over %s" % (curUnion.name))
+                continue # skip main union
+
             self.stdout.write("foreningen %s " % (curUnion.name))
             department = mainDepartment
-            name = "Forenings medlemskab: %s" % (curUnion.name)
+            name = "Forenings medlemskab 2018: %s" % (curUnion.name)
             open_hours = "-"
-            #responsible_name = "Coding Pirates"
-            #responsible_contact = "kontakt@codingpirates.dk"
-            #placename = ""
-            #zipcode = "5000"
-            #city = "Odense"
-            #streetname = "Seebladsgade"
-            #housenumber = "1"
-            #floor = ""
-            #door = ""
             dawa_id = ""
 
             localDepartments = str.join(", ", Department.objects.filter(union=curUnion).values_list("name", flat=True))
-            description = """Denne aktivitet er en særlig aktivitet oprettet for at kunne tilmelde sig som medlem med stemmeret i foreningen Coding Pirates
 
-            Du behøver IKKE tilmelde dig denne aktivitet hvis
-            1: Du har et barn der ønsker at deltage i en Coding Pirates aktivitet. (denne aktivitet giver ikke adgang til klubaften eller andre aktiviteter)
-            2: Du ønsker at stemme som værge for dit barn, der allerede er tilmeldt klubaften.
+            description = """Denne aktivitet er en særlig aktivitet oprettet for, at frivillige i Coding Pirates kan melde sig ind i vores forening og dermed få stemmeret i foreningen Coding Pirates %s samt Coding Pirates Denmark. 
             
-            Hvis dit barn er medlem af foreningen (tilmeldt og betalt for ugentlig klubaktivitet) kan du stemme på barnets vejne ved at tilmelde barnet til aktiviteten "fuldmagt ved general forsamling".
-            
-            Ved at tilmelde dig denne aktivitet, bliver du betalende medlem i foreningen Coding Pirates %s og kan dermed stemme til general forsamlingen.
-            
-            Flg. lokalafdelinger hører under %s:
-            
-            %s
-            """ % (curUnion.name, curUnion.name, localDepartments)
+Vær opmærksom på, at enkelte lokalforeninger ikke kræver, at frivillige er indmeldt som medlem for at have stemmeret, men for alle gælder, at man først har stemmeret i Coding Pirates Denmark, hvis man er meldt ind i sin lokalforening. 
 
+Du behøver derfor IKKE tilmelde dig denne aktivitet, hvis du har et barn, der er medlem af Coding Pirates og som ønsker at deltage i en Coding Pirates aktivitet. (denne aktivitet giver IKKE adgang til klubaften eller andre aktiviteter).
+
+Ved at tilmelde dig denne aktivitet bliver du altså betalende medlem i foreningen Coding Pirates %s og kan dermed stemme til generalforsamlingen i din lokalforening og i hovedforeningen Coding Pirates Denmark. 
+
+Flg. lokalafdelinger hører under %s:
+
+%s
+""" % (curUnion.name, curUnion.name, curUnion.name, localDepartments)
             instructions = ""
             start_date = datetime.date(year=2018, month=1, day=1)
-            end_date = datetime.date(year=2018, month=12, day=31)
-            signup_closing = datetime.date(year=2018, month=4, day=1)
+            end_date = datetime.date(year=2018, month=12, day=1)
+            signup_closing = datetime.date(year=2018, month=4, day=30)
             open_invite = True
             price_in_dkk = 75
             max_participants = 9999
@@ -53,6 +46,7 @@ class Command(BaseCommand):
             min_age = 16
 
             activity = Activity(department = department,
+                                union = curUnion,
                                 name=name,
                                 open_hours = open_hours,
                                 responsible_name = curUnion.chairman,
