cf0ee099fa1304164e53e185c9e42c8813395e5d
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2018-12-03 14:36:18 +0100

    Changes to design

--- forenings_medlemmer/settings.py
+++ forenings_medlemmer/settings.py
@@ -25,6 +25,7 @@ TEMPLATES = [
                 # list if you haven't customized them:
                 'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.debug',
+                'django.core.context_processors.request',
                 'django.template.context_processors.i18n',
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
