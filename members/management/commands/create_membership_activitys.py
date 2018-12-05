d718e5cfd529b723459ffb7e22cff3b7444755a9
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2018-12-05 11:37:10 +0100

    Lots of formatting while testing stuff related to users.

--- members/management/commands/create_membership_activitys.py
+++ members/management/commands/create_membership_activitys.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand, CommandError
 from members.models import Union, Activity, Department
 import datetime
 
+
 class Command(BaseCommand):
     help = 'Creates an activity pr. union to sign up for membership'
     output_transaction = True
@@ -13,7 +14,7 @@ class Command(BaseCommand):
         for curUnion in Union.objects.all():
             if curUnion.id == 1:
                 print("springer over %s" % (curUnion.name))
-                continue # skip main union
+                continue  # skip main union
 
             self.stdout.write("foreningen %s " % (curUnion.name))
             department = mainDepartment
@@ -23,13 +24,13 @@ class Command(BaseCommand):
 
             localDepartments = str.join(", ", Department.objects.filter(union=curUnion).values_list("name", flat=True))
 
-            description = """Denne aktivitet er en særlig aktivitet oprettet for, at frivillige i Coding Pirates kan melde sig ind i vores forening og dermed få stemmeret i foreningen Coding Pirates %s samt Coding Pirates Denmark. 
-            
-Vær opmærksom på, at enkelte lokalforeninger ikke kræver, at frivillige er indmeldt som medlem for at have stemmeret, men for alle gælder, at man først har stemmeret i Coding Pirates Denmark, hvis man er meldt ind i sin lokalforening. 
+            description = """Denne aktivitet er en særlig aktivitet oprettet for, at frivillige i Coding Pirates kan melde sig ind i vores forening og dermed få stemmeret i foreningen Coding Pirates %s samt Coding Pirates Denmark.
+
+Vær opmærksom på, at enkelte lokalforeninger ikke kræver, at frivillige er indmeldt som medlem for at have stemmeret, men for alle gælder, at man først har stemmeret i Coding Pirates Denmark, hvis man er meldt ind i sin lokalforening.
 
 Du behøver derfor IKKE tilmelde dig denne aktivitet, hvis du har et barn, der er medlem af Coding Pirates og som ønsker at deltage i en Coding Pirates aktivitet. (denne aktivitet giver IKKE adgang til klubaften eller andre aktiviteter).
 
-Ved at tilmelde dig denne aktivitet bliver du altså betalende medlem i foreningen Coding Pirates %s og kan dermed stemme til generalforsamlingen i din lokalforening og i hovedforeningen Coding Pirates Denmark. 
+Ved at tilmelde dig denne aktivitet bliver du altså betalende medlem i foreningen Coding Pirates %s og kan dermed stemme til generalforsamlingen i din lokalforening og i hovedforeningen Coding Pirates Denmark.
 
 Flg. lokalafdelinger hører under %s:
 
@@ -45,30 +46,30 @@ Flg. lokalafdelinger hører under %s:
             max_age = 99
             min_age = 16
 
-            activity = Activity(department = department,
-                                union = curUnion,
+            activity = Activity(department=department,
+                                union=curUnion,
                                 name=name,
-                                open_hours = open_hours,
-                                responsible_name = curUnion.chairman,
-                                responsible_contact = curUnion.chairman_email,
-                                placename = curUnion.placename,
-                                zipcode = curUnion.zipcode,
-                                city = curUnion.city,
-                                streetname = curUnion.streetname,
-                                housenumber = curUnion.housenumber,
-                                floor = curUnion.floor,
-                                door = curUnion.door,
-                                dawa_id = dawa_id,
-                                description = description,
-                                instructions = instructions,
-                                start_date = start_date,
-                                end_date = end_date,
-                                signup_closing = signup_closing,
-                                open_invite = open_invite,
-                                price_in_dkk = price_in_dkk,
-                                max_participants = max_participants,
-                                max_age = max_age,
-                                min_age = min_age
+                                open_hours=open_hours,
+                                responsible_name=curUnion.chairman,
+                                responsible_contact=curUnion.chairman_email,
+                                placename=curUnion.placename,
+                                zipcode=curUnion.zipcode,
+                                city=curUnion.city,
+                                streetname=curUnion.streetname,
+                                housenumber=curUnion.housenumber,
+                                floor=curUnion.floor,
+                                door=curUnion.door,
+                                dawa_id=dawa_id,
+                                description=description,
+                                instructions=instructions,
+                                start_date=start_date,
+                                end_date=end_date,
+                                signup_closing=signup_closing,
+                                open_invite=open_invite,
+                                price_in_dkk=price_in_dkk,
+                                max_participants=max_participants,
+                                max_age=max_age,
+                                min_age=min_age
                                 )
 
             activity.save()
--- members/management/commands/create_person_users.py
+++ members/management/commands/create_person_users.py
@@ -2,16 +2,17 @@ from django.core.management.base import BaseCommand
 from members.models.person import Person
 from django.contrib.auth.models import User
 
+
 class Command(BaseCommand):
     help = 'Creates a User for every Person that is of type PARENT or GUARDIAN and has a unique email'
 
     def handle(self, *args, **options):
         for person in Person.objects.filter(membertype__in=[Person.PARENT, Person.GUARDIAN]):
             if person.user:
-                continue # Person already has a user
+                continue  # Person already has a user
 
             if person.email and Person.objects.filter(email=person.email, membertype__in=[Person.PARENT, Person.GUARDIAN]).count() > 1:
-                continue # Ignore people with duplicate email
+                continue  # Ignore people with duplicate email
 
             user = User.objects.create_user(
                 username=person.email,
