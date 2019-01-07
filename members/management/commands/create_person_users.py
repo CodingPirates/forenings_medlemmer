26f59acb3eb8b5ee576a55da24265ca8fb5192be
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2019-01-07 14:54:08 +0100

    Password reset feature done

--- members/management/commands/create_person_users.py
+++ members/management/commands/create_person_users.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from members.models.person import Person
 from django.contrib.auth.models import User
+import random
 
 
 class Command(BaseCommand):
