dff7acf6809dfb32d66ed857e46e70d39941568d
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2019-02-02 18:20:38 +0100

    Fix bug in personupdate

--- members/views/PersonUpdate.py
+++ members/views/PersonUpdate.py
@@ -1,5 +1,5 @@
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
@@ -7,17 +7,23 @@ from members.forms import PersonForm
 
 from members.views.UpdatePersonFromForm import UpdatePersonFromForm
 
+from members.models.person import Person
+
 from members.utils.user import user_to_person
 
 
 @login_required
 def PersonUpdate(request, id):
-    person = user_to_person(request.user)
-    if request.method == 'POST':
-        form = PersonForm(request.POST, instance=person)
-        if form.is_valid():
-            UpdatePersonFromForm(person, form)
-            return HttpResponseRedirect(reverse('family_detail'))
+    person = Person.objects.get(pk=id)
+    persons_in_family = Person.objects.filter(family=user_to_person(request.user).family)
+    if person in persons_in_family:
+        if request.method == 'POST':
+            form = PersonForm(request.POST, instance=person)
+            if form.is_valid():
+                UpdatePersonFromForm(person, form)
+                return HttpResponseRedirect(reverse('family_detail'))
+        else:
+            form = PersonForm(instance=person)
+        return render(request, 'members/person_create_or_update.html', {'form': form, 'person': person})
     else:
-        form = PersonForm(instance=person)
-    return render(request, 'members/person_create_or_update.html', {'form': form, 'person': person})
+        return HttpResponse("Du kan kun redigere en person i din egen familie")
