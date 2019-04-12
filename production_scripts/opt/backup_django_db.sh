5c55177d8a40d7a8d6a5588f09af19b9e9760790
Benjamin <benjamin@rotendahl.dk>
2019-04-12 15:25:37 +0200

    Removed production scripts, not needed on heroku

--- production_scripts/opt/backup_django_db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-/usr/bin/sqlite3 /opt/database/db.sqlite3 ".dump" | bzip2 > /opt/database_backup/backup-$(date +%y-%m-%d-%H%M%S).sql.bz2
-
-find /opt/database_backup -mtime +30 -exec rm {} \;
-
