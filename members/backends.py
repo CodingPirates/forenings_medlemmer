af745b19de14f380865a4f1e05dd501296a96e2b
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2019-05-08 11:05:09 +0200

    Fiks login i forhold til opdateret Django version.

--- members/backends.py
+++ members/backends.py
@@ -1,18 +1,19 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 
 
 class CaseInsensitiveModelBackend(ModelBackend):
-    def authenticate(self, username=None, password=None):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        UserModel = get_user_model()
+        if username is None:
+            username = kwargs.get(UserModel.USERNAME_FIELD)
         try:
-            user = User.objects.get(username__iexact=username)
-            if user.check_password(password):
+            case_insensitive_username_field = '{}__iexact'.format(UserModel.USERNAME_FIELD)
+            user = UserModel._default_manager.get(**{case_insensitive_username_field: username})
+        except UserModel.DoesNotExist:
+            # Run the default password hasher once to reduce the timing
+            # difference between an existing and a non-existing user (#20760).
+            UserModel().set_password(password)
+        else:
+            if user.check_password(password) and self.user_can_authenticate(user):
                 return user
-        except User.DoesNotExist:
-            return None
-
-    def get_user(self, user_id):
-        try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
-            return None
