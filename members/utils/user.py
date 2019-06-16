1eb37fe00d9b275936d5ce2fb7875500200fc660
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2019-06-16 13:06:55 +0200

    Fix bug with missing admin person on all pages

--- members/utils/user.py
+++ members/utils/user.py
@@ -1,8 +1,17 @@
 from members.models.person import Person
 
+from django.core.exceptions import ObjectDoesNotExist
+
 
 def user_to_person(user):
-    q = Person.objects.filter(user=user)
-    if not q.exists():
+    try:
+        return Person.objects.filter(user=user).get()
+    except Person.DoesNotExist:
         return None
-    return q.get()
+
+def has_user(user):
+    person = user_to_person(user)
+    if person == None:
+        return False
+    else:
+        return True
--- members/views/FamilyDetails.py
+++ members/views/FamilyDetails.py
@@ -2,7 +2,7 @@ import datetime
 from django.conf import settings
 from django.shortcuts import render
 from django.utils import timezone
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 from members.models.activity import Activity
 from members.models.activityinvite import ActivityInvite
@@ -10,17 +10,15 @@ from members.models.activityparticipant import ActivityParticipant
 from members.models.department import Department
 from members.models.person import Person
 from members.models.waitinglist import WaitingList
-from members.utils.user import user_to_person
+from members.utils.user import user_to_person, has_user
 from django.shortcuts import redirect
 from django.urls import reverse
 
 
 @login_required
+@user_passes_test(has_user, '/admin_signup/')
 def FamilyDetails(request):
-    person = user_to_person(request.user)
-    if person == None:
-        return redirect(reverse("admin_signup"))
-    family = person.family
+    family = user_to_person(request.user).family
     invites = ActivityInvite.objects.filter(
         person__family=family, expire_dtm__gte=timezone.now(), rejected_dtm=None
     )
