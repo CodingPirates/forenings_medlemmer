93ca9a448edc52317279c120d1732d19f594fe90
Benjamin Bræstrup Sayoc <benj5378@outlook.com>
2019-06-20 15:54:14 +0200

    Update members/views/departmentViewFamily.py
    Co-Authored-By: Kristoffer Rath Hansen <kristoffer@rathhansen.com>

--- members/views/departmentViewFamily.py
+++ members/views/departmentViewFamily.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core import serializers
 
 from members.models.department import Department
