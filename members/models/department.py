c3eb29315d456788498ab9805dd448f2fef0db8f
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2019-06-26 13:53:47 +0200

    Removed print statements

--- members/models/department.py
+++ members/models/department.py
@@ -128,11 +128,7 @@ class Department(models.Model):
                     self.longitude = address["geometry"]["coordinates"][0]
                     self.latitude = address["geometry"]["coordinates"][1]
                     self.save()
-                    print("Opdateret for " + self.name)
-                    print("Updated coordinates for " + self.name)
                 except Exception as error:
-                    print("Couldn't find coordinates for " + self.name)
-                    print("Error " + str(error))
                     return None
 
         if self.latitude is not None and self.longitude is not None:
