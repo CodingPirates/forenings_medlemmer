7b21a0662354377f7b04345c5e0ad9b6c33a3fc3
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2019-07-29 11:57:46 +0200

    Migration updated

--- members/migrations/0007_auto_20190627_1635.py
+++ members/migrations/0007_auto_20190627_1635.py
@@ -7,31 +7,51 @@ from django.db import migrations
 def move_person(apps, schema_editor):
     Person = apps.get_model('members', 'Person')
     Address = apps.get_model('members', 'Address')
-    for person in Person.objects.all():
-        if person.address_moved == False:
-            if person.dawa_id in Address.objects.values_list('dawa_id', flat=True):
-                address_obj = Address.objects.get(dawa_id=person.dawa_id)
-                person.postal_address = address_obj
-                person.address_moved = True
-                person.save()
-            else:
-                # Address not already in Address table. Create new record
-                new_address = Address.objects.create(
-                    streetname = person.streetname,
-                    housenumber = person.housenumber,
-                    floor = person.floor,
-                    door = person.door,
-                    city = person.city,
-                    zipcode = person.zipcode,
-                    municipality = person.municipality,
-                    placename = person.placename,
-                    longitude = person.longitude,
-                    latitude = person.latitude,
-                    dawa_id = person.dawa_id,
-                    address_invalid = person.address_invalid
-                )
-                person.address_moved = True
-                person.save()
+    for person in Person.objects.filter(address_moved=False):
+        if person.dawa_id in Address.objects.values_list('dawa_id', flat=True):
+            address_obj = Address.objects.get(dawa_id=person.dawa_id)
+            person.postal_address = address_obj
+            person.address_moved = True
+            person.save()
+        else:
+            # Address not already in Address table. Create new record
+            new_address = Address.objects.create(
+                streetname = person.streetname,
+                housenumber = person.housenumber,
+                floor = person.floor,
+                door = person.door,
+                city = person.city,
+                zipcode = person.zipcode,
+                municipality = person.municipality,
+                placename = person.placename,
+                longitude = person.longitude,
+                latitude = person.latitude,
+                dawa_id = person.dawa_id,
+                address_invalid = person.address_invalid
+            )
+            person.address_moved = True
+            person.save()
+
+
+def reverse_move_person(apps, schema_editor):
+    Person = apps.get_model('members', 'Person')
+    Address = apps.get_model('members', 'Address')
+    for address in Address.objects.all():
+        for person in Person.objects.filter(postal_address=address.pk):
+            person.streetname = address.streetname,
+            person.housenumber = address.housenumber,
+            person.floor = address.floor,
+            person.door = address.door,
+            person.city = address.city,
+            person.zipcode = address.zipcode,
+            person.municipality = address.municipality,
+            person.placename = address.placename,
+            person.longitude = address.longitude,
+            person.latitude = address.latitude,
+            person.dawa_id = address.dawa_id,
+            person.address_invalid = address.address_invalid
+            person.address_moved = False
+            person.save()
 
 
 class Migration(migrations.Migration):
@@ -41,4 +61,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(move_person, reverse_move_person),
     ]
