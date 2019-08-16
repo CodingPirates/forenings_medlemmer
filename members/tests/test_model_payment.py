aff2e0a2cb162c0eab11fbb6cc67097360f54d7d
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2019-08-16 11:05:42 +0200

    Additional test

--- members/tests/test_model_payment.py
+++ members/tests/test_model_payment.py
@@ -9,3 +9,11 @@ from members.tests.factories import PaymentFactory, TIMEZONE
 class TestModelPayment(TestCase):
     def test_can_create_payment(self):
         PaymentFactory() # no error expected
+
+    def test_can_create_refund(self):
+        # Create a payment
+        payment = PaymentFactory(status="NEW", amount_ore=50000)
+
+        # Create a refund
+        PaymentFactory(external_id=payment.external_id, status="REFUND", amount_ore=-50000)
+        # should not give any errors
