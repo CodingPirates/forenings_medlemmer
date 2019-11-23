5e11ebdba6d44f1f4fd5e3e7a078f0cf434bf038
benj5378 <benj5378@outlook.com>
2019-11-23 21:50:29 +0100

    Vis kun relevante afdelinger til opskrivning (ikke dem man allerede er opskrevet)

--- members/templatetags/waiting_list.py
+++ members/templatetags/waiting_list.py
@@ -1,5 +1,7 @@
 from django import template
 from members.models.waitinglist import WaitingList
+from members.models.department import Department
+from members.models.person import Person
 
 register = template.Library()
 
@@ -7,3 +9,11 @@ register = template.Library()
 @register.simple_tag
 def number_on_waiting_list(row):
     return row.number_on_waiting_list()
+
+
+@register.simple_tag
+def already_on_waitinglist(person, department):
+    if WaitingList.objects.filter(person=person, department=department):
+        return True
+    else:
+        return False
