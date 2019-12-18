7f76a4900ca06c113650260c5ee675cc977372d3
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2019-12-19 00:38:04 +0100

    Fix tests

--- members/tests/test_model_payment.py
+++ members/tests/test_model_payment.py
@@ -4,9 +4,15 @@ from members.models.payment import Payment
 from datetime import datetime
 from freezegun import freeze_time
 
-from members.tests.factories import PaymentFactory
+from members.tests.factories import PaymentFactory, TIMEZONE
 
 
 class TestModelPayment(TestCase):
+    def mock_reference_time(self, time):
+        def mock(timezone):
+            pass
+
+        return mock
+
     def test_can_create_payment(self):
         PaymentFactory() # should not throw errors
