be9b7aa681535368506bf9343803a743673ae48a
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2019-12-30 17:05:25 +0100

    Cleaned migration and added utils back

---
+++ members/utils/address.py
@@ -0,0 +1,12 @@
+def format_address(streetname, housenumber, floor=None, door=None):
+    address = streetname + " " + housenumber
+    if floor != "" and door != "":
+        address = address + ", " + floor + ". " + door + "."
+
+    if floor != "" and door == "":
+        address = address + ", " + floor + "."
+
+    if floor == "" and door != "":
+        address = address + ", " + door + "."
+
+    return address
