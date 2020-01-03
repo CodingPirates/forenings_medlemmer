25eb70edc880c74a5ce7ae8681c76e010d72d348
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2020-01-03 13:41:26 +0100

    Black formatting

--- members/migrations/0012_auto_20200103_1328.py
+++ members/migrations/0012_auto_20200103_1328.py
@@ -8,23 +8,25 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('members', '0011_adminuserinformation_unions'),
+        ("members", "0011_adminuserinformation_unions"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='adminuserinformation',
-            name='departments',
-            field=models.ManyToManyField(blank=True, to='members.Department'),
+            model_name="adminuserinformation",
+            name="departments",
+            field=models.ManyToManyField(blank=True, to="members.Department"),
         ),
         migrations.AlterField(
-            model_name='adminuserinformation',
-            name='unions',
-            field=models.ManyToManyField(blank=True, to='members.Union'),
+            model_name="adminuserinformation",
+            name="unions",
+            field=models.ManyToManyField(blank=True, to="members.Union"),
         ),
         migrations.AlterField(
-            model_name='adminuserinformation',
-            name='user',
-            field=models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            model_name="adminuserinformation",
+            name="user",
+            field=models.OneToOneField(
+                on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL
+            ),
         ),
     ]
