987cdca06c37bef171ee0bb8bed85245d34dfd8d
Benjamin Bræstrup Sayoc <benj5378@outlook.com>
2020-01-28 17:29:12 +0100

    Flake8

--- members/views/Overview.py
+++ members/views/Overview.py
@@ -1,15 +1,8 @@
-import datetime
-from django.conf import settings
 from django.shortcuts import render
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required, user_passes_test
 
-from members.models.activity import Activity
 from members.models.activityinvite import ActivityInvite
-from members.models.activityparticipant import ActivityParticipant
-from members.models.department import Department
-from members.models.person import Person
-from members.models.waitinglist import WaitingList
 from members.utils.user import user_to_person, has_user
 
 
