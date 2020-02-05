476a256f5623828f2116cf665b2b06a10a1ea245
Benjamin Bræstrup Sayoc <benj5378@outlook.com>
2020-02-05 17:16:00 +0200

    Fiks fejl i aktivitetsmodellens test

--- members/tests/test_model_activity.py
+++ members/tests/test_model_activity.py
@@ -11,27 +11,43 @@ class TestModelActivity(TestCase):
     def setUp(self):
         self.activity = ActivityFactory(min_age=8, max_age=12)
 
-        birthday = fake.date_time_between(
-            start_date="-" + str(self.activity.max_age) + "y",
-            end_date="-" + str(self.activity.min_age) + "y",
-        )
-        self.applicablePersons = PersonFactory.create_batch(50, birthday=birthday)
+        self.applicablePersons = [
+            PersonFactory(
+                birthday=fake.date_between(
+                    start_date="-"
+                    + str(int((self.activity.max_age + 1) * 365.24 - 1))
+                    + "d",
+                    end_date="-" + str(self.activity.min_age) + "y",
+                )
+            )
+            for i in range(0, 50)
+        ]
 
-        birthday = fake.date_time_between(
-            start_date="-" + str(self.activity.min_age) + "y",
-            end_date="-" + str(self.activity.min_age + 10) + "y",
-        )
-        self.not_applicablePersons_tooYoung = PersonFactory.create_batch(
-            50, birthday=birthday
-        )
+        print("-" + str(int((self.activity.max_age + 1) * 365.24 - 1)) + "d")
+        print("-" + str(self.activity.min_age) + "y")
 
-        birthday = fake.date_time_between(
-            start_date="-" + str(self.activity.max_age + 10) + "y",
-            end_date="-" + str(self.activity.max_age) + "y",
-        )
-        self.not_applicablePersons_tooOld = PersonFactory.create_batch(
-            50, birthday=birthday
-        )
+        self.not_applicablePersons_tooYoung = [
+            PersonFactory(
+                birthday=fake.date_time_between(
+                    start_date="-" + str(int(self.activity.min_age * 365.24 + 1)) + "d",
+                    end_date="+2y",  # str(-(self.activity.min_age - 10)) + "y",
+                ),
+            )
+            for i in range(0, 50)
+        ]
+
+        print("-" + str(int(self.activity.min_age * 365.24 + 1)) + "d")
+        print("+2y",)
+
+        self.not_applicablePersons_tooOld = [
+            PersonFactory(
+                birthday=fake.date_time_between(
+                    start_date="-" + str(self.activity.max_age + 10) + "y",
+                    end_date="-" + str(int((self.activity.max_age + 1) * 365.24)) + "d",
+                ),
+            )
+            for i in range(0, 50)
+        ]
 
     # Modified from django.test.testcases.TransactionTestCase.assertQuerysetEqual
     def assertQuerysetDiffernt(
