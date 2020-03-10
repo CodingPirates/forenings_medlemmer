c0d9a7df504cae679356bab6d4679104af0bd554
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2020-03-10 12:37:24 +0100

    Updated pages, tabs for waitlist and mobile tables

---
+++ members/static/members/js/tab.js
@@ -0,0 +1,40 @@
+// Sets data-label attribute on tables
+document.addEventListener("DOMContentLoaded", function(event) {
+  for (var tabs of Array.from(document.getElementsByClassName("tabs"))) {
+    const tabButtons = Array.from(
+      Array.from(tabs.children).filter(element => element.tagName === "UL")[0]
+        .children
+    );
+    const sections = Array.from(
+      Array.from(tabs.children).filter(element => element.tagName === "SECTION")
+    );
+    console.log(sections);
+    // Default to first as active
+    toggleActive(sections, tabButtons, 0);
+
+    for (var button of tabButtons) {
+      button.addEventListener("click", event =>
+        toggleActive(sections, tabButtons, tabButtons.indexOf(event.srcElement))
+      );
+    }
+  }
+});
+
+function toggleActive(sections, buttons, activeIndex) {
+  // remove active from every button.
+  for (var button of buttons) {
+    button.className = button.className
+      .split(" ")
+      .filter(className => className !== "tab-active")
+      .join(" ");
+  }
+  for (var section of sections) {
+    section.className = section.className
+      .split(" ")
+      .filter(className => className !== "tab-section-active")
+      .join(" ");
+  }
+
+  buttons[activeIndex].className += " tab-active";
+  sections[activeIndex].className += " tab-section-active";
+}
