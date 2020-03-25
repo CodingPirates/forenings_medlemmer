68771dcc4015ac0dcb28bb1bf1b2237c0fe7e16d
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2020-03-25 15:20:24 +0100

    Updated test

--- members/tests/test_functional/test_admin_can_load.py
+++ members/tests/test_functional/test_admin_can_load.py
@@ -3,8 +3,10 @@ import os
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from django.contrib.auth.models import User
+from members.tests.factories import PersonFactory
 
 """
 This test creates a super user and checks that the admin interface can be loaded
@@ -15,6 +17,7 @@ class SignUpTest(StaticLiveServerTestCase):
     host = socket.gethostbyname(socket.gethostname())
 
     def setUp(self):
+        self.person = PersonFactory.create()
         self.password = "Miss1337"
         self.name = "kaptajn hack"
         self.admin = User.objects.create_superuser(
@@ -53,4 +56,25 @@ class SignUpTest(StaticLiveServerTestCase):
                 )
             ),
             0,
+        )
+
+        # Check that person admin can load
+        elment = self.browser.find_element_by_link_text("Personer")
+        self.browser.get(elment.get_attribute("href"))
+        try:
+            WebDriverWait(self.browser, 10).until(EC.url_contains("person"))
+        except Exception:
+            self.fail("Could not reach person admin site")
+
+        # GO to person change page
+        elment = self.browser.find_element_by_link_text(str(self.person))
+        self.browser.get(elment.get_attribute("href"))
+        try:
+            WebDriverWait(self.browser, 10).until(EC.url_contains("change"))
+        except Exception:
+            self.fail("Could not reach person admin site")
+
+        self.assertEqual(
+            self.browser.find_element_by_name("email").get_attribute("value"),
+            self.person.email,
         )
