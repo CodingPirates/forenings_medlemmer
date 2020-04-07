fee2f54f4e8be79f5980cc203dc42cd6e50a9d96
Jacob Haubjerg Madsen <jacob_h@live.dk>
2020-04-07 12:24:17 +0200

    Temp work on new Department_list (DOESN'T WORK YET)

--- members/views/departmentView.py
+++ members/views/departmentView.py
@@ -7,22 +7,12 @@ from members.models.address import Address
 
 @xframe_options_exempt
 def departmentView(request, unique=None):
-    depQuery = (
-        Department.objects.filter(closed_dtm__isnull=True)
-        .filter(isVisible=True)
-        .exclude(address__region="")
-    )
-    deps = {}
-    for region in Address.REGION_CHOICES:
-        deps[region[1]] = []
-
-    for department in depQuery:
-        coordinates = (department.address.latitude, department.address.longitude)
-        dep = {"html": department.toHTML(), "isVisible": department.isVisible}
-        if None not in coordinates:
-            dep["latitude"] = str(coordinates[0])
-            dep["longtitude"] = str(coordinates[1])
-        else:
-            dep["isVisible"] = False
-        deps[department.address.get_region_display()].append(dep)
-    return render(request, "members/department_list.html", {"departments": deps})
+    departments = Department.get_open_departments()
+    departments = [
+        department
+        for department in departments
+        if department.address.region != ""
+        and department.isVisible
+        and None not in (department.address.latitude, department.address.longitude)
+    ]
+    return render(request, "members/department_list.html", {"departments": departments})
