ba079fd104c50cdcca65ecb2f752f253001d4cd5
Jacob Haubjerg Madsen <jacob_h@live.dk>
2020-05-04 13:39:42 +0200

    small changes

--- members/views/unionMembersView.py
+++ members/views/unionMembersView.py
@@ -8,26 +8,22 @@ from members.utils.user import has_user
 @login_required
 @user_passes_test(has_user, "/admin_signup/")
 def unionMembersView(request, union_id):
-    # get chosen union
     union = get_object_or_404(Union, pk=union_id)
 
-    # Check if user is admin of union
-    access = Union.user_union_leader(union, request.user)
+    user_has_access = Union.user_union_leader(union, request.user)
 
-    # get members of union
-    if access:
+    if user_has_access:
         members = list(set(union.members()))
 
-    # get years union has been active
     today = timezone.now().date()
-    years = range(union.founded.year, today.year + 1)
+    years_active = range(union.founded.year, today.year + 1)
 
     context = {
-        "years": years,
+        "years": years_active,
         "current_year": today.year,
         "members": members,
         "union": union,
-        "access": access,
+        "access": user_has_access,
     }
 
     return render(request, "members/union_members.html", context)
