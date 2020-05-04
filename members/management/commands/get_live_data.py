f33f2e8455ad0bef1454dba4034b505beeef2110
Jacob Haubjerg Madsen <jacob_h@live.dk>
2020-05-04 13:35:36 +0200

    flake8

--- members/management/commands/get_live_data.py
+++ members/management/commands/get_live_data.py
@@ -1,6 +1,5 @@
 from zipfile import ZipFile
 import os
-import json
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 from django.db import DatabaseError
