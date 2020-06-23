b3a908afe07ac3f9db269925436f5f80acc8035b
Jacob Haubjerg Madsen <jacob_h@live.dk>
2020-06-23 13:47:26 +0200

    Fixes non-visible departments still showing up on the map

--- members/views/departmentView.py
+++ members/views/departmentView.py
@@ -11,7 +11,11 @@ def departmentView(request, unique=None):
         "members/department_list.html",
         {
             "departments": filter(
-                lambda dep: dep.address.region != "", Department.get_open_departments()
+                lambda dep: dep.isVisible,
+                filter(
+                    lambda dep: dep.address.region != "",
+                    Department.get_open_departments(),
+                ),
             )
         },
     )
