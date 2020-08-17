0c0372a42b1884c89381da6eadb43f6ecac047f5
Benjamin <benjamin@rotendahl.dk>
2020-08-17 13:15:54 +0200

    Updated deps (#591)

--- members/tests/factories/activity_participant_factory.py
+++ members/tests/factories/activity_participant_factory.py
@@ -3,7 +3,8 @@ from members.tests.factories.factory_helpers import TIMEZONE
 from members.models import ActivityParticipant
 from members.tests.factories.activity_factory import ActivityFactory
 from members.tests.factories.member_factory import MemberFactory
-from factory import Faker, DjangoModelFactory, SubFactory
+from factory import Faker, SubFactory
+from factory.django import DjangoModelFactory
 
 
 class ActivityParticipantFactory(DjangoModelFactory):
