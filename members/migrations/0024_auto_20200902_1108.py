3b316d52f2a3b81060857c984976b09f89eb586f
Kristoffer Rath Hansen <kristoffer@rathhansen.com>
2020-09-02 11:16:13 +0200

    Update black on migration

--- members/migrations/0024_auto_20200902_1108.py
+++ members/migrations/0024_auto_20200902_1108.py
@@ -6,13 +6,17 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('members', '0023_address_dawa_overwrite'),
+        ("members", "0023_address_dawa_overwrite"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='address',
-            name='dawa_overwrite',
-            field=models.BooleanField(default=False, help_text='\n    Lader dig gemme en anden Længdegrad og breddegrad end den gemt i DAWA     (hvor vi henter adressedata).     Spørg os i #medlemsssystem_support på Slack hvis du mangler hjælp.\n    ', verbose_name='Overskriv DAWA'),
+            model_name="address",
+            name="dawa_overwrite",
+            field=models.BooleanField(
+                default=False,
+                help_text="\n    Lader dig gemme en anden Længdegrad og breddegrad end den gemt i DAWA     (hvor vi henter adressedata).     Spørg os i #medlemsssystem_support på Slack hvis du mangler hjælp.\n    ",
+                verbose_name="Overskriv DAWA",
+            ),
         ),
     ]
