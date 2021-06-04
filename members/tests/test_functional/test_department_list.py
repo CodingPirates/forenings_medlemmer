1bb3d27e9b644739ae7d36b8528bea2916d437b6
Jarl Friis <jarl@softace.dk>
2021-06-04 21:51:41 +0200

    Red: iframe view shall not showheader

--- members/tests/test_functional/test_department_list.py
+++ members/tests/test_functional/test_department_list.py
@@ -58,6 +58,7 @@ class DepartmentListTest(StaticLiveServerTestCase):
         self.assertEqual("Coding Pirates Medlemssystem", self.browser.title)
         self.browser.save_screenshot("test-screens/department_list_1.png")
 
+        self.assertEquals(0, len(self.browser.find_elements_by_tag_name("nav")))
         # check that there's the "Hovedstaden" region tab
         self.browser.find_element_by_xpath(
             "//div[@class='tabs']/ul/li[text()[contains(.,'Region Hovedstaden')]]"
