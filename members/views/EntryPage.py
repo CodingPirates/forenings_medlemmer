13ee4897b5c523da4c14e722a370cc691f7648db
Benjamin Bræstrup Sayoc <benj5378@outlook.com>
2022-06-27 21:11:26 +0200

    Notifikationer (#436)
    * Overview i sin egen fil
    
    * Tilføj notifikationer til oversigts- og familiesiden
    
    * Black-fis og oprydning
    
    * Flake8
    
    * Flyt notifikationer over i en template
    
    * Omnavngivning af CSS-fil
    
    * Simplificer gennemgang
    
    * Fjern container
    
    * Updated notifications
    
    * Center align on phones
    
    * Layoutopdatering
    
    * fixed tests
    
    * added help text to notification version of entry page
    
    * Update notifications to work on latest UI/views
    
    * Remove old overview page, site has been restructured since this PR was originally created
    Including css files
    
    * Allow to decline invitation from activity signup form
    
    * Python style fixes
    
    Co-authored-by: Benjamin Rotendahl <Benjamin@Rotendahl.dk>
    Co-authored-by: Jacob Haubjerg Madsen <jacob_h@live.dk>
    Co-authored-by: Kristoffer Rath Hansen <kristoffer@rathhansen.com>
    Co-authored-by: rasmusselsmark <rasmus@selsmark.dk>

--- members/views/EntryPage.py
+++ members/views/EntryPage.py
@@ -1,7 +1,23 @@
 from django.views.decorators.clickjacking import xframe_options_exempt
 from django.shortcuts import render
+from django.utils import timezone
+
+from members.models.activityinvite import ActivityInvite
+from members.utils.user import user_to_person
 
 
 @xframe_options_exempt
 def EntryPage(request):
-    return render(request, "members/entry_page.html")
+    if not request.user.is_anonymous:
+        family = user_to_person(request.user).family
+        invites = ActivityInvite.objects.filter(
+            person__family=family, expire_dtm__gte=timezone.now(), rejected_dtm=None
+        )
+
+        context = {
+            "invites": invites,
+        }
+    else:
+        context = {}
+
+    return render(request, "members/entry_page.html", context)
