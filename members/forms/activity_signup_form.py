55474cfc1bd55dc2fc99d758e68de7f37a302617
Kristoffer Rath Hansen <kristoffer@rathhansen.com>
2022-08-14 15:52:22 +0200

    Remove decline button from form temporarily so it works (#734)
    * Remove from form
    
    * Ensure we don't include activityinvite

--- members/forms/activity_signup_form.py
+++ members/forms/activity_signup_form.py
@@ -59,9 +59,6 @@ class ActivitySignupForm(forms.Form):
                             Submit(
                                 "submit", "Tilmeld og betal", css_class="button-success"
                             ),
-                            HTML(
-                                "<a href='{%url 'invitation_decline' family.unique invitation.id %}'><button type='button' class='button-danger'>Afslå invitationen</button></a>",
-                            ),
                             HTML("<a href='{% url 'family_detail' %}'>Tilbage</a>"),
                         ),
                     ),
