af1cffc7f72eff26f5b1f14d24a3164a87a5d3d4
Kristoffer Rath Hansen <kristoffer@rathhansen.com>
2022-10-30 13:58:39 +0100

    Django 4.1.2 [WIP]

--- forenings_medlemmer/urls.py
+++ forenings_medlemmer/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'foreningsmedlemmer.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r"^admin/", admin.site.urls),
-    url(r"^", include("members.urls")),
-    url("^sentry-debug/", lambda request: 1 / 0),  # Test url, delete this
+    path(r"^admin/", admin.site.urls),
+    path(r"^", include("members.urls")),
+    path("^sentry-debug/", lambda request: 1 / 0),  # Test url, delete this
 ]
--- members/urls.py
+++ members/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 from members.views import (
     AccountCreate,
     Activities,
@@ -25,15 +25,15 @@ from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    url(r"^$", EntryPage, name="entry_page"),
-    url(r"^graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
-    url(r"^account/create/$", AccountCreate, name="account_create"),
-    url(
+    path(r"^$", EntryPage, name="entry_page"),
+    path(r"^graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path(r"^account/create/$", AccountCreate, name="account_create"),
+    path(
         r"^account/login/$",
         auth_views.LoginView.as_view(template_name="members/login.html"),
         name="person_login",
     ),
-    url(
+    path(
         r"^account/forgot/$",
         auth_views.PasswordResetView.as_view(template_name="members/forgot.html"),
         {
@@ -43,75 +43,75 @@ urlpatterns = [
         },
         name="password_reset",
     ),
-    url(
+    path(
         r"^account/forgot/done/$",
         auth_views.PasswordResetDoneView.as_view(
             template_name="members/forgot_done.html"
         ),
         name="password_reset_done",
     ),
-    url(
+    path(
         r"^account/forgot/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$",
         auth_views.PasswordResetConfirmView.as_view(
             template_name="members/forgot_confirm.html"
         ),
         name="password_reset_confirm",
     ),
-    url(
+    path(
         r"^account/forgot/complete/$",
         auth_views.PasswordResetCompleteView.as_view(
             template_name="members/forgot_complete.html"
         ),
         name="password_reset_complete",
     ),
-    url(
+    path(
         r"^account/logout/$",
         auth_views.LogoutView.as_view(template_name="members/logout.html"),
         {"next_page": "/"},
         name="person_logout",
     ),
-    url(r"^activities/$", Activities, name="activities"),
-    url(r"^membership/$", Membership, name="membership"),
-    url(r"^support_membership/$", SupportMembership, name="support_membership"),
-    url(r"^volunteer$", volunteerSignup, name="volunteer_signup"),
-    url(r"^user_created/$", userCreated, name="user_created"),
-    url(r"^admin_signup/$", AdminSignup, name="admin_signup"),
-    url(r"^family/$", FamilyDetails, name="family_detail"),
-    url(r"^family/Person/(?P<id>[\d]+)/$", PersonUpdate, name="person_update"),
-    url(r"^family/Person/(?P<membertype>[A-Z]{2})$", PersonCreate, name="person_add"),
-    url(
+    path(r"^activities/$", Activities, name="activities"),
+    path(r"^membership/$", Membership, name="membership"),
+    path(r"^support_membership/$", SupportMembership, name="support_membership"),
+    path(r"^volunteer$", volunteerSignup, name="volunteer_signup"),
+    path(r"^user_created/$", userCreated, name="user_created"),
+    path(r"^admin_signup/$", AdminSignup, name="admin_signup"),
+    path(r"^family/$", FamilyDetails, name="family_detail"),
+    path(r"^family/Person/(?P<id>[\d]+)/$", PersonUpdate, name="person_update"),
+    path(r"^family/Person/(?P<membertype>[A-Z]{2})$", PersonCreate, name="person_add"),
+    path(
         r"^family/activity/(?P<activity_id>[\d]+)/person/(?P<person_id>[\d]+)/$",
         ActivitySignup,
         name="activity_signup",
     ),
-    url(
+    path(
         r"^family/activity/(?P<activity_id>[\d]+)/person/(?P<person_id>[\d]+)/view/$",
         ActivitySignup,
         name="activity_view_person",
     ),
-    url(
+    path(
         r"^family/activity/(?P<activity_id>[\d]+)/view/$",
         ActivitySignup,
         name="activity_view_family",
     ),
-    url(
+    path(
         r"^family/(?P<unique>[\w-]+)/invitation_decline/(?P<invitation_id>[\d]+)/$",
         DeclineInvitation,
         name="invitation_decline",
     ),
-    url(
+    path(
         r"^family/payment_gateway_error$",
         paymentGatewayErrorView,
         name="payment_gateway_error_view",
     ),
-    url(r"^confirm_details/$", ConfirmFamily, name="confirm_details"),
-    url(
+    path(r"^confirm_details/$", ConfirmFamily, name="confirm_details"),
+    path(
         r"^waiting_list/(?P<id>[\d]+)/(?P<departmentId>[\d]+)/(?P<action>(subscribe|unsubscribe))/$",
         WaitingListSetSubscription,
         name="waiting_list_subscription",
     ),
-    url(r"^activity/(?P<activity_id>[\d]+)/$", ActivitySignup, name="activity_view"),
-    url(r"^quickpay_callback$", QuickpayCallback, name="quickpay_callback"),
-    url(r"^department_signup$", DepartmentSignView, name="department_signup"),
-    url(r"^departments$", departmentView, name="department_view"),
+    path(r"^activity/(?P<activity_id>[\d]+)/$", ActivitySignup, name="activity_view"),
+    path(r"^quickpay_callback$", QuickpayCallback, name="quickpay_callback"),
+    path(r"^department_signup$", DepartmentSignView, name="department_signup"),
+    path(r"^departments$", departmentView, name="department_view"),
 ]
