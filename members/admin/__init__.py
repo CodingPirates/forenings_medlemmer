30ef6326d7f2b4eb84e0ae2bd69621396348f475
Mikael Hewel <mikael@hewel.dk>
2022-10-30 13:36:13 +0100

    Update __init__.py
    blake

--- members/admin/__init__.py
+++ members/admin/__init__.py
@@ -6,6 +6,7 @@ from django.db.models import Q
 
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
+
 # from django.contrib.auth.admin import UserAdmin
 from django.db.models.functions import Lower
 from django.http import HttpResponse
