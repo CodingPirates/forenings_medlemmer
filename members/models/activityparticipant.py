caa7d142693e74425699a2e54e5a0181324324b7
mhewel <58829786+mhewel@users.noreply.github.com>
2022-12-12 22:24:40 +0100

    Update activityparticipant.py (#764)
    restored to previous version , to have activity name listed after person in the ActivityParticipant column in Admin > Payments view
    
    Co-authored-by: Kristoffer Rath Hansen <kristoffer@rathhansen.com>

--- members/models/activityparticipant.py
+++ members/models/activityparticipant.py
@@ -41,9 +41,7 @@ class ActivityParticipant(models.Model):
     )
 
     def __str__(self):
-        return self.member.__str__()
-        # + ", " + self.activity.name
-        # No reason to show an activity here - looks like it's the first activity for the given user
+        return self.member.__str__() + ", " + self.activity.name
 
     def paid(self):
         # not paid if unconfirmed payments on this activity participation
