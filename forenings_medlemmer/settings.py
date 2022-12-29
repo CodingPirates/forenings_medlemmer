70b69b0a19ca067fb902a92025362c3aa22c8987
Jonathan <jonathan@bangert.dk>
2022-12-29 14:09:05 +0100

    Make min lenght of paswd 10

--- forenings_medlemmer/settings.py
+++ forenings_medlemmer/settings.py
@@ -143,7 +143,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
         'OPTIONS': {
-            'min_length': 9,
+            'min_length': 10,
         }
     },
     {
