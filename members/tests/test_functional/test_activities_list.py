a7e9e4ea888fc47a9876089d2f1b3933d8749805
Mikael Hewel <Mikael@hewel.dk>
2023-01-24 10:42:40 +0100

    changed div to section in test criteria

--- members/tests/test_functional/test_activities_list.py
+++ members/tests/test_functional/test_activities_list.py
@@ -38,7 +38,7 @@ class ActivitiesListTest(StaticLiveServerTestCase):
         # check that the test activity is present
         activity_name = self.browser.find_element(
             By.XPATH,
-            "//div[@class='tabs']/section[@id='current_activities']/div[@class='tabs']/div[@class='activity-per-region']/table/tbody/tr[1]/td[@data-label='Aktivitet']",
+            "//div[@class='tabs']/section[@id='current_activities']/div[@class='tabs']/section[@class='activity-per-region']/table/tbody/tr[1]/td[@data-label='Aktivitet']",
         ).get_attribute("textContent")
         self.assertEqual(activity_name, self.activity_1.name)
 
