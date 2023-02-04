f338a15a3478487978607c60c5b2527b0a53fb4f
Kristoffer Rath Hansen <kristoffer@rathhansen.com>
2023-02-04 23:57:03 +0100

    Remove unused statement

--- members/models/activity.py
+++ members/models/activity.py
@@ -3,7 +3,6 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
 
 
 class Activity(models.Model):
