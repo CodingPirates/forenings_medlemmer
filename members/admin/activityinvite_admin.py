207f02a29463f15702172862dfcd2532ecfa260f
Mikael Hewel <Mikael@hewel.dk>
2023-02-10 19:05:09 +0100

    Added filters, changed some columns

--- members/admin/activityinvite_admin.py
+++ members/admin/activityinvite_admin.py
@@ -1,13 +1,16 @@
 from django import forms
 from django.contrib import admin
 from django.db.models.functions import Lower
+from django.urls import reverse
 from django.utils import timezone
+from django.utils.safestring import mark_safe
 
 from members.models import (
     Activity,
     ActivityInvite,
     AdminUserInformation,
     Department,
+    Union,
     Person,
 )
 
@@ -42,17 +45,108 @@ class ActivivtyInviteActivityListFilter(admin.SimpleListFilter):
             return queryset.filter(activity__pk=self.value())
 
 
+class ActivityInviteUnionListFilter(admin.SimpleListFilter):
+    title = "Lokalforeninger"
+    parameter_name = "activity__department__union"
+
+    def lookups(self, request, model_admin):
+        return [(str(union.pk), str(union.name)) for union in Union.objects.all()]
+
+    def queryset(self, request, queryset):
+        if self.value() is None:
+            return queryset
+        else:
+            return queryset.filter(activity__department__union__pk=self.value())
+
+
+class ActivityInviteDepartmentListFilter(admin.SimpleListFilter):
+    title = "Afdelinger"
+    parameter_name = "activity__department"
+
+    def lookups(self, request, model_admin):
+        return [
+            (str(department.pk), str(department.name))
+            for department in Department.objects.all()
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() is None:
+            return queryset
+        else:
+            return queryset.filter(activity__department__pk=self.value())
+
+
+class ActivitInviteListCurrentFilter(admin.SimpleListFilter):
+    title = "Nuværende og kommende aktiviteter"
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = "activity"
+
+    def lookups(self, request, model_admin):
+        activitys = []
+        for act in Activity.objects.filter(
+            department__in=AdminUserInformation.get_departments_admin(request.user),
+            end_date__gte=timezone.now(),
+        ).order_by("department__name", "-start_date"):
+            activitys.append((str(act.pk), str(act)))
+        return activitys
+
+    def queryset(self, request, queryset):
+        if self.value() is None:
+            return queryset
+        else:
+            return queryset.filter(activity=self.value())
+
+
+class ActivitInviteListFinishedFilter(admin.SimpleListFilter):
+    title = "Tidligere aktiviteter"
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = "activity"
+
+    def lookups(self, request, model_admin):
+        activitys = []
+        for act in Activity.objects.filter(
+            department__in=AdminUserInformation.get_departments_admin(request.user),
+            end_date__lte=timezone.now(),
+        ).order_by("department__name", "-start_date"):
+            activitys.append((str(act.pk), str(act)))
+        return activitys
+
+    def queryset(self, request, queryset):
+        if self.value() is None:
+            return queryset
+        else:
+            return queryset.filter(activity=self.value())
+
+
 class ActivityInviteAdmin(admin.ModelAdmin):
     list_display = (
-        "person",
-        "activity",
+        "activity_department_union_link",
+        "activity_department_link",
+        "activity_link",
+        "person_link",
+        # "activity",
         "invite_dtm",
+        "expire_dtm",
         "person_age_years",
         "person_zipcode",
         "rejected_at",
     )
-    list_filter = (ActivivtyInviteActivityListFilter,)
-    search_fields = ("person__name",)
+    list_filter = (
+        ActivityInviteUnionListFilter,
+        ActivityInviteDepartmentListFilter,
+        ActivivtyInviteActivityListFilter,
+        ActivitInviteListCurrentFilter,
+        ActivitInviteListFinishedFilter,
+    )
+    date_hierarchy = "activity__start_date"
+    search_fields = (
+        "activity__department__union__name",
+        "activity__department__name",
+        "activity__name",
+        "person__name",
+    )
     list_display_links = None
     form = ActivityInviteAdminForm
 
@@ -101,3 +195,41 @@ class ActivityInviteAdmin(admin.ModelAdmin):
         return item.person.zipcode
 
     person_zipcode.short_description = "Postnummer"
+
+    def activity_department_union_link(self, item):
+        url = reverse(
+            "admin:members_union_change", args=[item.activity.department.union_id]
+        )
+        link = '<a href="%s">%s</a>' % (url, item.activity.department.union.name)
+        return mark_safe(link)
+
+    activity_department_union_link.short_description = "Forening"
+    activity_department_union_link.admin_order_field = (
+        "activity__department__union__name"
+    )
+
+    def activity_department_link(self, item):
+        url = reverse(
+            "admin:members_department_change", args=[item.activity.department_id]
+        )
+        link = '<a href="%s">%s</a>' % (url, item.activity.department.name)
+        return mark_safe(link)
+
+    activity_department_link.short_description = "Afdeling"
+    activity_department_link.admin_order_field = "activity__department__name"
+
+    def activity_link(self, item):
+        url = reverse("admin:members_activity_change", args=[item.activity.id])
+        link = '<a href="%s">%s</a>' % (url, item.activity.name)
+        return mark_safe(link)
+
+    activity_link.short_description = "Aktivitet"
+    activity_link.admin_order_field = "activity__name"
+
+    def person_link(self, item):
+        url = reverse("admin:members_person_change", args=[item.person_id])
+        link = '<a href="%s">%s</a>' % (url, item.person.name)
+        return mark_safe(link)
+
+    person_link.short_description = "Person"
+    person_link.admin_order_field = "person__name"
