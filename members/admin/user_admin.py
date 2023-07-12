2ffc43e7ea30ef77f2cf34e57e6f77b164606d42
mhewel <58829786+mhewel@users.noreply.github.com>
2023-07-12 17:01:10 +0200

    Rettede filtre for Grupper,  Forening og Afdelinger
    Tilføjede "ingen" til alle 3 filtre og "mindst en" for forenings- og afdelings filtrene

--- members/admin/user_admin.py
+++ members/admin/user_admin.py
@@ -21,7 +21,7 @@ class AdminUserGroupListFilter(admin.SimpleListFilter):
     parameter_name = "group"
 
     def lookups(self, request, model_admin):
-        groupList = ()
+        groupList = [("none", "(ingen gruppe)")]
         for aGroup in Group.objects.all().order_by("name"):
             groupList += (
                 (
@@ -33,6 +33,8 @@ class AdminUserGroupListFilter(admin.SimpleListFilter):
 
     def queryset(self, request, queryset):
         group_id = request.GET.get(self.parameter_name, None)
+        if group_id == "none":
+            return queryset.filter(groups__isnull=True)
         if group_id:
             return queryset.filter(groups=group_id)
         return queryset
@@ -43,7 +45,7 @@ class AdminUserUnionListFilter(admin.SimpleListFilter):
     parameter_name = "union"
 
     def lookups(self, request, model_admin):
-        unionList = ()
+        unionList = [("none", "(ingen forening)"), ("any", "(mindst en forening)")]
         for aUnion in Union.objects.all().order_by("name"):
             unionList += (
                 (
@@ -55,8 +57,12 @@ class AdminUserUnionListFilter(admin.SimpleListFilter):
 
     def queryset(self, request, queryset):
         union_id = request.GET.get(self.parameter_name, None)
-        if union_id:
-            return queryset.filter(groups=union_id)
+        if union_id == "none":
+            return queryset.filter(adminuserinformation__unions__isnull=True)
+        elif union_id == "any":
+            return queryset.exclude(adminuserinformation__unions__isnull=True)
+        elif union_id:
+            return queryset.filter(adminuserinformation__unions=union_id)
         return queryset
 
 
@@ -65,7 +71,7 @@ class AdminUserDepartmentListFilter(admin.SimpleListFilter):
     parameter_name = "department"
 
     def lookups(self, request, model_admin):
-        departmentList = ()
+        departmentList = [("none", "(ingen afdeling)"), ("any", "(mindst en afdeling)")]
         for aDepartment in Department.objects.all().order_by("name"):
             departmentList += (
                 (
@@ -76,9 +82,13 @@ class AdminUserDepartmentListFilter(admin.SimpleListFilter):
         return departmentList
 
     def queryset(self, request, queryset):
-        union_id = request.GET.get(self.parameter_name, None)
-        if union_id:
-            return queryset.filter(groups=union_id)
+        department_id = request.GET.get(self.parameter_name, None)
+        if department_id == "none":
+            return queryset.filter(adminuserinformation__departments__isnull=True)
+        elif department_id == "any":
+            return queryset.exclude(adminuserinformation__departments__isnull=True)
+        if department_id:
+            return queryset.filter(adminuserinformation__departments=department_id)
         return queryset
 
 
