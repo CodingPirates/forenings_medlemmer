96f0117c7fb2e2446088cb71949ec94e24f5419c
Rasmus Selsmark <rasmus@selsmark.dk>
2024-07-21 10:52:39 +0200

    Verify that activity end_date has been set, before validating it

--- members/models/activity.py
+++ members/models/activity.py
@@ -120,7 +120,8 @@ class Activity(models.Model):
         min_amount = self.NO_MINIMUM_AMOUNT
 
         # Issue 1058: If activity is in the past then skip this check
-        if self.end_date > timezone.now().date():
+        # During activity creation, the end_date could have been left empty
+        if self.end_date and self.end_date > timezone.now().date():
             if activitytype == "FORENINGSMEDLEMSKAB":
                 min_amount = self.MEMBERSHIP_MIN_AMOUNT
 
