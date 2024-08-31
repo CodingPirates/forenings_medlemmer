3fb294bfae3b321b76a9bf5c9f2251ab79b573db
Kristoffer Rath Hansen <kristoffer@rathhansen.com>
2024-08-31 15:16:11 +0200

    Include missing function

--- members/models/family.py
+++ members/models/family.py
@@ -48,6 +48,9 @@ class Family(models.Model):
             return None
         return parent
 
+    def get_children(self):
+        return Person.objects.filter(family=self, membertype=Person.CHILD)
+
     def get_persons(self):
         return Person.objects.filter(family=self)
 
