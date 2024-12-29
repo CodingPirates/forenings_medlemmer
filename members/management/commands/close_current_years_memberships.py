d54555afe0e371a278f83744ec492f65b67de8b6
Kristoffer Rath Hansen <kristoffer@rathhansen.com>
2024-12-29 15:03:11 +0100

    Implement command to close current years membership activities

---
+++ members/management/commands/close_current_years_memberships.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from members.models.union import Union
+from members.models.activity import Activity
+from members.models.activitytype import ActivityType
+from members.models.department import Department
+import datetime
+
+
+class Command(BaseCommand):
+    help = "Closes memberships for this year"
+    output_transaction = True
+
+    def handle(self, *args, **options):
+        for curMembershipActivity in Activity.objects.filter(activitytype="FORENINGSMEDLEMSKAB").filter(start_date=datetime.date(year=2024, month=1, day=1)):
+            curMembershipActivity.visible=False
+            curMembershipActivity.save()
