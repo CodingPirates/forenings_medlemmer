2600df118135ba8b161b11d137cccd91d149ae43
Kristoffer Rath Hansen <kristoffer@rathhansen.com>
2025-04-14 00:23:24 +0200

    Update DAWA when person is saved

--- members/models/municipality.py
+++ members/models/municipality.py
@@ -1,4 +1,5 @@
 from django.db import models
+import requests
 
 
 class Municipality(models.Model):
@@ -15,3 +16,30 @@ class Municipality(models.Model):
         verbose_name = "Kommune"
         verbose_name_plural = "Kommuner"
         ordering = ["name"]
+
+    @staticmethod
+    def get_municipality_for_person(person) -> bool:
+        # Call the API to get municipality id
+        url = f"https://api.dataforsyningen.dk/adresser?id={person.dawa_id}"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            if not data:
+                return False
+
+            municipality_id = data[0]["adgangsadresse"]["kommune"]["kode"]
+
+            if municipality_id:
+                try:
+                    # Look up the found municipality id from Municipality model
+                    municipality = Municipality.objects.get(dawa_id=municipality_id)
+                    # Set the municipality for the person
+                    person.municipality = municipality
+                    person.save()
+                    return True
+                except Municipality.DoesNotExist:
+                    return False
+
+        return False
