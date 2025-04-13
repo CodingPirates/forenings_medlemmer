1abecf6133a1a7a1b29f301fe4b5c21c78596c0d
Kristoffer Rath Hansen <kristoffer@rathhansen.com>
2025-04-14 00:28:50 +0200

    Remove unused function

--- members/models/municipality.py
+++ members/models/municipality.py
@@ -1,5 +1,4 @@
 from django.db import models
-import requests
 
 
 class Municipality(models.Model):
@@ -16,30 +15,3 @@ class Municipality(models.Model):
         verbose_name = "Kommune"
         verbose_name_plural = "Kommuner"
         ordering = ["name"]
-
-    @staticmethod
-    def get_municipality_for_person(person) -> bool:
-        # Call the API to get municipality id
-        url = f"https://api.dataforsyningen.dk/adresser?id={person.dawa_id}"
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            data = response.json()
-
-            if not data:
-                return False
-
-            municipality_id = data[0]["adgangsadresse"]["kommune"]["kode"]
-
-            if municipality_id:
-                try:
-                    # Look up the found municipality id from Municipality model
-                    municipality = Municipality.objects.get(dawa_id=municipality_id)
-                    # Set the municipality for the person
-                    person.municipality = municipality
-                    person.save()
-                    return True
-                except Municipality.DoesNotExist:
-                    return False
-
-        return False
