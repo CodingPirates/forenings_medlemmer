70fabf16281843da87305cfb23b8275cba70ff5f
Kristoffer Rath Hansen <kristoffer@rathhansen.com>
2025-09-14 14:56:58 +0200

    Add handling of activity price

--- members/management/commands/recalculate_activityparticipant_price.py
+++ members/management/commands/recalculate_activityparticipant_price.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from members.models.activity import Activity
 from members.models.activityinvite import ActivityInvite
 from members.models.activityparticipant import ActivityParticipant
 
@@ -8,6 +9,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for activity_participant in ActivityParticipant.objects.all():
+            #Find matching activity
+            activity = Activity.objects.filter(
+                id=activity_participant.activity_id
+            ).first()
             # Find matching activity invite
             activity_invite = ActivityInvite.objects.filter(
                 activity=activity_participant.activity,
@@ -15,4 +20,6 @@ class Command(BaseCommand):
             ).first()
             if activity_invite:
                 activity_participant.price_in_dkk = activity_invite.price_in_dkk
+            else:
+                activity_participant.price_in_dkk = activity.price_in_dkk
             activity_participant.save()
