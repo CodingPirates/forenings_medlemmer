4cdd5bc71ec83bb1b9adf5a3dc58c2978ae36e38
Kristoffer Rath Hansen <kristoffer@rathhansen.com>
2025-11-15 23:22:41 +0100

    Black on settings

--- forenings_medlemmer/settings.py
+++ forenings_medlemmer/settings.py
@@ -75,9 +75,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 ALLOWED_HOSTS = [host.replace(" ", "") for host in env.list("ALLOWED_HOSTS")]
 BASE_URL = os.environ["BASE_URL"]
-assert not BASE_URL.endswith("/"), (
-    f"BASE_URL environment variable must not end with '/'. It is set to '{BASE_URL}'."
-)
+assert not BASE_URL.endswith(
+    "/"
+), f"BASE_URL environment variable must not end with '/'. It is set to '{BASE_URL}'."
 
 INSTALLED_APPS = (
     "django_bootstrap5",
