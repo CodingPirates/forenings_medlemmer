f9dc6f2722aed7e899d7c52a93404234d4be17a8
Kristoffer Rath Hansen <kristoffer@rathhansen.com>
2025-11-15 23:18:50 +0100

    Finish up middleware

--- members/middleware/last_login_middleware.py
+++ members/middleware/last_login_middleware.py
@@ -9,6 +9,13 @@ class LastLoginMiddleware:
 
     def __call__(self, request):
         if request.user.is_authenticated:
+            # Update user's last login time
+            request.user.last_login = timezone.now()
+            request.user.save()
+
+            # Update family's last visit time
             family = Person.objects.get(user=request.user).family
             family.last_visit_at = timezone.now()
             family.save()
+
+        return self.get_response(request)
