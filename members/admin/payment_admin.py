48a3846c3c2d41e2e2b8ea0e4062a915f57c104b
mhewel <58829786+mhewel@users.noreply.github.com>
2026-01-17 11:40:26 +0100

    Admin > Payment object: Brug autocomplete istedet for raw-id-dropdown

--- members/admin/payment_admin.py
+++ members/admin/payment_admin.py
@@ -3,6 +3,25 @@ from django.contrib import admin
 
 
 class PaymentAdmin(admin.ModelAdmin):
+    fields = [
+        "pk",
+        "added_at",
+        "payment_type",
+        "activityparticipant",
+        "activity",
+        "member",
+        "family",
+        "person",
+        "amount_ore",
+        "body_text",
+        "accepted_at",
+        "confirmed_at",
+        "cancelled_at",
+        "refunded_at",
+        "rejected_at",
+        "rejected_message",
+        "reminder_sent_at",
+    ]
     list_per_page = settings.LIST_PER_PAGE
 
     list_display = [
@@ -16,8 +35,23 @@ class PaymentAdmin(admin.ModelAdmin):
         "rejected_at",
         "activityparticipant",
     ]
-    list_filter = ["payment_type", "activity"]
-    raw_id_fields = ("person", "activityparticipant", "family", "member")
+
+    readonly_fields = ["pk"]
+    list_filter = [
+        "payment_type",
+        "activity",
+    ]
+    # raw_id_fields = ("person", "family", "member")
     date_hierarchy = "added_at"
     search_fields = ("family__email",)
     select_related = "activityparticipant"
+    autocomplete_fields = (
+        "activity",
+        "activityparticipant",
+        "member",
+        "person",
+        "family",
+    )
+
+    class Media:
+        css = {"all": ("members/css/custom_admin.css",)}
