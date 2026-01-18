e023781572ac9a8f0bcbb4eed89ea41498cccb1d
Kristoffer Rath Hansen <kristoffer@rathhansen.com>
2026-01-18 17:05:44 +0100

    Comment out correctly

--- members/tests/test_functional/test_admin_department.py
+++ members/tests/test_functional/test_admin_department.py
@@ -7,9 +7,9 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import Client
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.support.ui import Select
 from members.models import (
     Activity,
     ActivityType,
